0d7217b41536d6c777a26190419a031839a0c1ed
roberthughes <hughes.brewzz@gmail.com>
2022-02-07 14:01:59 -0700

    commit tree 63b53760ab9050c1e5bae71c8ec0aed62daae785

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+function appendCat(name) {
+    let copyCats = [...cats, "Broom"];
+   return copyCats;
+}
+ function prependCat(names)   {
+    let copyCat = ["Arnold", ...cats];
+    return copyCat;
+ }
+ function removeLastCat() {
+    let catCopy = cats.slice(-1);
+     return catCopy
+ }
+
+ function removeFirstCat()  {
+     let copCat = cats.slice(0);
+     return copCat;
+ }
+ 
